ddcfbcbb9c6df70b16ddfba6e8baa71604dbff2d
chuyanlong <a1008611abcd@126.com>
2025-05-07 10:57:57 +0800

    refactor: remove testing and jsdoc related snippets from javascript/typescript files

--- lua/vim-react-snippets/luasnippets/javascript.lua
+++ lua/vim-react-snippets/luasnippets/javascript.lua
@@ -3,27 +3,27 @@ local util = require("vim-react-snippets.util")
 local common = require("vim-react-snippets.common")
 local exports = require("vim-react-snippets.exports")
 local imports = require("vim-react-snippets.imports")
-local jsdoc = require("vim-react-snippets.jsdoc")
+-- local jsdoc = require("vim-react-snippets.jsdoc")
 local logging = require("vim-react-snippets.logging")
 local react_prop_types = require("vim-react-snippets.react-prop-types")
 local react_hooks = require("vim-react-snippets.react-hooks")
 local react_components = require("vim-react-snippets.react-components")
-local react_testing = require("vim-react-snippets.react-testing")
+-- local react_testing = require("vim-react-snippets.react-testing")
 local redux = require("vim-react-snippets.redux")
-local test_queries = require("vim-react-snippets.test-queries")
-local testing = require("vim-react-snippets.testing")
+-- local test_queries = require("vim-react-snippets.test-queries")
+-- local testing = require("vim-react-snippets.testing")
 
 return util.merge_lists(
   imports(),
   exports(),
   logging(),
   common(false),
-  jsdoc(false),
+  -- jsdoc(false),
   react_hooks(false),
   redux(false),
-  testing(),
-  test_queries(),
+  -- testing(),
+  -- test_queries(),
   react_components(false),
-  react_testing(false),
+  -- react_testing(false),
   react_prop_types(false)
 )
--- lua/vim-react-snippets/luasnippets/typescript.lua
+++ lua/vim-react-snippets/luasnippets/typescript.lua
@@ -3,12 +3,12 @@ local util = require("vim-react-snippets.util")
 local common = require("vim-react-snippets.common")
 local exports = require("vim-react-snippets.exports")
 local imports = require("vim-react-snippets.imports")
-local jsdoc = require("vim-react-snippets.jsdoc")
+-- local jsdoc = require("vim-react-snippets.jsdoc")
 local logging = require("vim-react-snippets.logging")
 local react_hooks = require("vim-react-snippets.react-hooks")
 local redux = require("vim-react-snippets.redux")
-local test_queries = require("vim-react-snippets.test-queries")
-local testing = require("vim-react-snippets.testing")
+-- local test_queries = require("vim-react-snippets.test-queries")
+-- local testing = require("vim-react-snippets.testing")
 
 local ls = require("luasnip")
 
@@ -21,11 +21,11 @@ return util.merge_lists(
   exports(),
   logging(),
   common(true),
-  jsdoc(true),
+  -- jsdoc(true),
   react_hooks(true),
   redux(true),
-  testing(),
-  test_queries(),
+  -- testing(),
+  -- test_queries(),
   {
     s({
       trig = "intf",
